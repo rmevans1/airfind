04da9e7daa958a6aec0c127bbc6fb15ac96251a9
Robert Evans <robertevans@robertevansmb.com>
2017-09-19 16:43:30 -0400

    Fix AppController.js

--- client/controllers/AppController.js
+++ client/controllers/AppController.js
@@ -1,5 +1,5 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('AppController', ['$scope', '$http'], function($scope, $http){
+myApp.controller('AppController', ['$scope', '$http', function($scope, $http){
     console.log('AppController Initialized...');
-});
+}]);
