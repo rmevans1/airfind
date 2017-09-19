d61f5dc76ec1a9ab24987f2b77937ac8327ba003
Robert Evans <robertevans@robertevansmb.com>
2017-09-19 18:51:08 -0400

    Add find airports by prox

--- client/controllers/AppController.js
+++ client/controllers/AppController.js
@@ -16,4 +16,19 @@ myApp.controller('AppController', ['$scope', '$http', function($scope, $http){
             $scope.airports = response;
         });
     }
+    
+    $scope.findAirportsByProx = function(){
+        var location = {
+            distance: $scope.location.distance
+        }
+        //had to change .success to .then .success no longer supported
+        $http.get('/geocode/location?address='+$scope.location.address).then(function(loc_response){
+            location.lat = loc_response.locations[0].latitude
+            location.llon = loc_response.locations[0].longitude
+            
+            $http.post('/api/airports/prox', location).then(function(response){
+                $scope.airports = response;
+            });
+        });
+    }
 }]);
