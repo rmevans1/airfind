7bf558235405826583aef0fa353deef5f5d809aa
Robert Evans <robertevans@robertevansmb.com>
2017-09-19 18:07:28 -0400

    Get airports by state in angular

--- client/controllers/AppController.js
+++ client/controllers/AppController.js
@@ -2,4 +2,10 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('AppController', ['$scope', '$http', function($scope, $http){
     console.log('AppController Initialized...');
+    
+    $scope.getAirports = function(){
+        $http.get('/api/airports').success(function(response){
+            $scope.airports = response;
+        });
+    }
 }]);
