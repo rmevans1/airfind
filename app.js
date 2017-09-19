f04bb9848115025ca9c2bd2e796c4ea7cee8c8db
Robert Evans <robertevans@robertevansmb.com>
2017-09-19 15:18:45 -0400

    Initialize express app

---
+++ app.js
@@ -0,0 +1,21 @@
+var localconfig = require('./localconfig');
+var express = require('express');
+var express_geocoding_api = require('express-geocoding-api');
+var bodyParser = require('body-parser');
+
+var app=express();
+var mongoose = require('mongoose');
+
+//mongoose connect
+mongoose.connect(localconfig.connection_url);
+var db = mongoose.connection;
+
+app.use(express.static(__dirname+'/client'));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+    res.send('Please use /api/airports or /api/states endpoints');
+});
+
+app.listen(3000);
+console.log('App started on port 3000');
