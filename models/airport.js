9faa12287fedb8dff926e8274d008026818aca15
Robert Evans <robertevans@robertevansmb.com>
2017-09-19 15:22:29 -0400

    Add airport model

---
+++ models/airport.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+
+//Airport Schema
+var airportSchema = mongoose.Schema({
+    loc: {
+        type:{
+            type: String
+        },
+        coordinates: {
+            type: Array
+        },
+        name: {
+            type: String
+        },
+        code: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }
+});
+
+var Airport = module.exports = mongoose.model('Airport', airportSchema);
+
+//Get Airpots
+module.exports.getAirports = function(callback, limit){
+    Airport.findOne(id, callback);
+}
