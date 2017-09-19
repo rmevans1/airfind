eee92563fadd763620ee0f25ba307fcb80a8a1c2
Robert Evans <robertevans@robertevansmb.com>
2017-09-19 18:27:52 -0400

    Fix controller for angular 1.6

--- client/controllers/AppController.js
+++ client/controllers/AppController.js
@@ -4,7 +4,8 @@ myApp.controller('AppController', ['$scope', '$http', function($scope, $http){
     console.log('AppController Initialized...');
     
     $scope.getAirports = function(){
-        $http.get('/api/airports').success(function(response){
+        //had to change .success to .then .success no longer supported
+        $http.get('/api/airports').then(function(response){
             $scope.airports = response;
         });
     }
